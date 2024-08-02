b8b64f7b1a24cbe2e291bf1621e6badcfbfa80c2
azx <zxAi@qq.com>
2024-08-02 14:30:04 +0000

    final version of Point Prompt record for ablation modification

--- MAE/utils/parser.py
+++ MAE/utils/parser.py
@@ -12,7 +12,7 @@ def get_args():
     parser.add_argument(
         '--cache_prompt', 
         action='store_true', 
-        default=True, 
+        default=False, 
         help = 'use cache enhancement')
     parser.add_argument(
         '--launcher',
@@ -35,7 +35,7 @@ def get_args():
         default=False, 
         help='whether to use sync bn')
     # some args
-    parser.add_argument('--exp_name', type = str, default='debug', help = 'experiment name') #femae-pointprompt-r-learningrate0.0005-per.aft.attn-point5
+    parser.add_argument('--exp_name', type = str, default='femae-pointprompt-r-learningrate0.0005-per.aft.attn-point5-perta0.4', help = 'experiment name') #femae-pointprompt-r-learningrate0.0005-per.aft.attn-point5
     parser.add_argument('--loss', type=str, default='cd1', help='loss name')
     parser.add_argument('--start_ckpts', type = str, default=None, help = 'reload used ckpt path')
     parser.add_argument('--ckpts', type = str, default='./pretrained_bases/femae-epoch-300.pth', help = 'test used ckpt path') #pretrained_bases/mae_base.pth ./pretrained_bases/recon_base.pth  ./pretrained_bases/femae-epoch-300.pth
@@ -90,7 +90,7 @@ def get_args():
         help = 'in the forward for block')
     ###
     parser.add_argument(
-        '--way', type=int, default=10)
+        '--way', type=int, default=5)
     parser.add_argument(
         '--shot', type=int, default=10)
     parser.add_argument(
